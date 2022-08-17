d95b6469a7fe901b07c4195f320b5b9fc7c8da54
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-08-17 21:02:15 +0900

    Add files via upload

---
+++ 문제19번.py
@@ -0,0 +1,25 @@
+# 1302번
+# 엄청 어려운문제..
+
+n = int(input())
+
+a = []
+
+d = dict()
+
+b = []
+
+for i in range(n):
+    a.append(input())
+
+for j in list(set(a)):
+    d[j] = a.count(j)
+
+for k in d.keys():
+    if d[k] == max(d.values()):
+        b.append(k)
+
+
+b.sort()
+
+print(b[0])
