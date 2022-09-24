d591fd57fa595f3f248efc1c1de48c4650f7b6c3
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-09-24 20:08:07 +0900

    Add files via upload

---
+++ 문제47번.py
@@ -0,0 +1,25 @@
+
+n = int(input())
+
+a = list(map(int,input().split()))
+
+ave = round(sum(a)/n)
+min = 2147000000
+
+for idx , x in enumerate(a):
+    tmp = abs(x-ave)
+
+    if tmp < min :
+        min = tmp
+        score = x
+        res = idx+1
+
+    elif tmp == min:
+        if x > score:
+            score = x
+            res = idx+1
+
+print(ave, res)
+
+
+
