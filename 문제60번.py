db899896340ec169bb9ec80b59d16c5f41d9e14b
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-10-15 17:03:51 +0900

    Add files via upload

--- 문제60번.py
+++ 문제60번.py
@@ -6,18 +6,26 @@ n = int(input())
 for i in range(n):
     s = input()
     s = s.upper()
-    size = len(s)
+    # size = len(s)
 
-    for j in range(size//2):
+    # for j in range(size//2):
 
-        if s[j] != s[-1-j]:
-            print("#%d NO"  %(i+1))
+    #     if s[j] != s[-1-j]:
+    #         print("#%d NO"  %(i+1))
 
-            break
+    #         break
 
+    # else:
+    #     print("#%d YES" %(i+1))
+
+
+
+    if s == s[::-1]:
+        print("#%d NO"  %(i+1))
     else:
-        print("#%d YES" %(i+1))
-        
+        print("#%d NO"  %(i+1))
+
+
 
 
         
